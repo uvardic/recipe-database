9393b897b093d716197b00141c11d00ce5963929
Uros Vardic <uros.vardiic@gmail.com>
2020-08-10 22:36:59 +0200

    Initial commit

---
+++ src/main/java/recipe/RecipeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeDatabaseApplication.class, args);
+    }
+
+}
